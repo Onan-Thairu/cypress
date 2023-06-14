adc823fe2681f93b83653cbed4e97e2da59ae61d
Onan Thairu <thairuonan@gmail.com>
2023-06-14 18:25:43 +0300

    chore: before, beforeEach, fixtures, baseUrl

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('todo mvc tests', () => {
+  let mainData
+
+  before(() => {
+    cy.fixture('getData').then((data) => {
+      mainData = data
+    })
+  })
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  afterEach(() => {
+    cy.screenshot()
+  })
+
+  it('Adds a todo', () => {
+    cy.get('.new-todo').type(mainData.firstItem + '{enter}')
+    cy.get('label').should('contain.text', mainData.firstItem)
+    cy.get('.toggle').should('not.be.checked')
+  })
+
+  it('Completes a todo', () => {
+    cy.get('.new-todo').type(mainData.secondItem + '{enter}')
+    cy.get('.toggle').click()
+    cy.get('.toggle').should('be.checked')
+  })
+})
