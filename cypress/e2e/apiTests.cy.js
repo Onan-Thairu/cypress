afb5083999dba3bdc7bb22310c89938f87ac2d23
Onan Thairu <thairuonan@gmail.com>
2023-06-15 10:27:50 +0300

    chore: create booking.

--- cypress/e2e/apiTests.cy.js
+++ cypress/e2e/apiTests.cy.js
@@ -15,4 +15,25 @@ describe('API tests', () =>{
             cy.log(bookingID)
         })
     })
+
+    it('Create booking', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://restful-booker.herokuapp.com/booking',
+            body: {
+                "firstname": "Onan",
+                "lastname": "Thairu",
+                "totalprice": 444,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2023-06-15",
+                    "checkout": "2023-06-20"
+                },
+                "additionalneeds": "Breakfast"
+            }
+        }).then(response => {
+            expect(response.status).to.eq(200)
+            cy.log(response.body)
+        })
+    })
 })
